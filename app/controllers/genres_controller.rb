eec0e03e0abc39fad736aa75dd24952fa3efb9b9
mmcgovern0 <mmcgovern.wework@gmail.com>
2019-05-20 16:48:47 -0400

    Done.

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,42 @@
+class GenresController < ApplicationController
+	
+	def index
+		@genres = Genre.all
+	end
+
+	def new
+		@genre = Genre.new
+	end
+
+	def create
+		@genre = Genre.create(genre_params)
+		redirect_to @genre
+	end
+
+	def show
+		@genre = Genre.find(params[:id])
+	end
+
+	def edit
+		@genre = Genre.find(params[:id])
+	end
+
+	def update
+		@genre = Genre.find(params[:id])
+		@genre.update(genre_params)
+		redirect_to @genre
+	end
+
+	def destroy
+		@genre = Genre.find(params[:id])
+		@genre.destroy
+		redirect_to genres_path
+	end
+
+	private
+
+	def genre_params
+		params.require(:genre).permit!
+	end
+	
+end
